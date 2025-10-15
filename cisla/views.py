9768af33289d35aea594d55408b9b087ccaf4ac9
Peter Macúš <petermacus134@gmail.com>
2025-10-15 10:04:00 +0200

    ach jaj

--- cisla/views.py
+++ cisla/views.py
@@ -5,19 +5,18 @@ def index(request):
     if request.method == "GET":
         vysledok = 0
     if request.method == "POST":
+        
+    try:
         a = int(request.POST["a"])
         b = int(request.POST["b"])
-        operator = request.POST["operator"]
-        if operator == "+":
-            vysledok = a + b
-        elif operator == "-":
-            vysledok = a - b 
-        elif operator == "*":
-            vysledok = a * b
-        else:
+                a = float(input("Zadaj prvé číslo: "))
+                b = float(input("Zadaj druhé číslo: "))
+            except ValueError:
+                print("Zadaná hodnota nie je číslo")
             vysledok = a / b
+       
 
-        return render(request, 'kalkulacka/index.html')
+
 
 
     return render(request, 'kalkulacka/index.html',dict(vysledok=vysledok))
