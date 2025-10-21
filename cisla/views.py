59b4fe771ddc063437576dc0bf3229b7ae539c77
Peťo <petermacus134@gmail.com>
2025-10-21 22:50:44 +0200

    fix na kalkulacku a cisla

--- cisla/views.py
+++ cisla/views.py
@@ -2,23 +2,27 @@ from django.shortcuts import render, HttpResponse
 from . import views
 
 def index(request):
-    if request.method == "GET":
-        vysledok = 0
-    if request.method == "POST":
-        
-    try:
-        a = int(request.POST["a"])
-        b = int(request.POST["b"])
-                a = float(input("Zadaj prvé číslo: "))
-                b = float(input("Zadaj druhé číslo: "))
-            except ValueError:
-                print("Zadaná hodnota nie je číslo")
-            vysledok = a / b
-       
-
-
-
-
-    return render(request, 'kalkulacka/index.html',dict(vysledok=vysledok))
-
-
+        if request.method == "GET":
+                vysledok = 0
+        if request.method == "POST":
+                try:
+                        a = int(request.POST['a'])
+                        b = int(request.POST['b'])
+                except:
+                        vysledok = ""
+                operator = request.POST['operator']
+                try:
+                        if operator == "+":
+                                vysledok = a+b
+                        elif operator == "-":
+                                vysledok = a-b
+                        elif operator == "*":
+                                vysledok = a*b
+                        elif operator == "/":
+                                try:
+                                        vysledok = a/b
+                                except:
+                                        vysledok="Nemozete delit nulou"
+                except:
+                        vysledok = "musite zadat cislo"
+        return render(request, 'cisla/index.html', dict(vysledok=vysledok))
--- kalkulacka/views.py
+++ kalkulacka/views.py
@@ -2,24 +2,53 @@ from django.shortcuts import render, HttpResponse
 from . import views
 
 def index(request):
-    if request.method == "GET":
-        vysledok = 0
-    if request.method == "POST":
-        a = int(request.POST["a"])
-        b = int(request.POST["b"])
-        operator = request.POST["operator"]
-        if operator == "+":
-            vysledok = a + b
-        elif operator == "-":
-            vysledok = a - b 
-        elif operator == "*":
-            vysledok = a * b
-        else:
-            vysledok = a / b
+        if request.method == "GET":
+                vacsie = ""
+                parneA = ""
+                parneB = ""
+                prvoA = ""
+                prvoB = ""
+        if request.method == "POST":
+            try:
+                a = int(request.POST['a'])
+                b = int(request.POST['b'])
+            except:
+                a = ""
+                b = ""
+        try:
+                if a>b:
+                    vacsie = "Cislo a je vacsie ako cislo b"
+                elif b>a:
+                    vacsie = "Cislo b je vacsie ako cislo a"
+                elif a==b:
+                    vacsie = "Cisla su rovne"
+                else:
+                    vacsie = "Error"
+                
+                parneA = parne(a)
+                parneB = parne(b)
 
-        return render(request, 'kalkulacka/index.html')
+                prvoA = prvocislo(a)
+                prvoB = prvocislo(b)
+        except:
+                vacsie = "Zadaj cislo"
+                parneA = ""
+                parneB = ""
+                prvoA = ""
+                prvoB = ""
+        return render(request, 'kalkulacka/index.html', {"vacsie":vacsie, "parneA": parneA, "parneB": parneB, "prvoA": prvoA, "prvoB": prvoB})
 
+def parne(cislo):
+      if cislo % 2 == 0:
+            return "Parne"
+      else:
+            return "Neparne"
+      
+def prvocislo(cislo):
+      if cislo == 1:
+           return "Nie je prvocislo"
 
-    return render(request, 'kalkulacka/index.html',dict(vysledok=vysledok))
-
-
+      for i in range(2, cislo):
+        if cislo%i == 0:
+             return "Nie je prvocislo"
+      return "Je prvocislo"
